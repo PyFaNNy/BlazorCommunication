3da2da95c73d369f0f2ce600e90d9830fce4fd0e
Artem Puzyrev <fanny.koob@gmail.com>
2023-07-16 19:24:38 +0300

    Initial Chat

---
+++ Presentation/BlazorCommunication.Client/Models/Chat/ChatMessage.cs
@@ -0,0 +1,10 @@
+﻿namespace BlazorCommunication.Client.Models.Chat;
+
+public class ChatMessageModel
+{
+    public string Message { get; set; }
+    public bool IsSender { get; set; }
+    public DateTime Time { get; set; }
+    
+    public ChatUserModel Sender { get; set; }
+}
---
+++ Presentation/BlazorCommunication.Client/Models/Chat/ChatUser.cs
@@ -0,0 +1,12 @@
+﻿namespace BlazorCommunication.Client.Models.Chat;
+
+public class ChatUserModel
+{
+    public Guid Id { get; set; }
+    public string FirstName { get; set; }
+    public string LastName { get; set; }
+    public string ImgURL { get; set; }
+    
+    public string LastMessage { get; set; }
+    public int Notifications { get; set; }
+}
